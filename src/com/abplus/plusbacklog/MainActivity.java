6555dadbf45fddba39a82d524f568ee01261d32f
kazhida <kazhida@abplus.com>
2013-04-02 23:04:50 +0900

    とりあえず、準備だけ

---
+++ src/com/abplus/plusbacklog/MainActivity.java
@@ -0,0 +1,15 @@
+package com.abplus.plusbacklog;
+
+import android.os.Bundle;
+import com.example.android.actionbarcompat.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
